949c35d14c18669da85f11d40da22996fa84f38d
rachitrawat <rachit.rc.rawat@gmail.com>
2017-01-05 16:07:28 +0530

    list test update

--- app/src/main/java/a122016/rr/com/alertme/PlaceAdapter.java
+++ app/src/main/java/a122016/rr/com/alertme/PlaceAdapter.java
@@ -49,28 +49,10 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
         // the default TextView.
         highwayTextView.setText(currentPlace.getHighwayNumber());
 
-        // Find the TextView in the list_item.xml layout with the ID default_text_view.
-        TextView f2015TextView = (TextView) listItemView.findViewById(R.id.f2015_text_view);
-        // Get the default translation from the currentPlace object and set this text on
-        // the default TextView.
-        f2015TextView.setText(String.valueOf(currentPlace.getFatalties2015()));
-
-        // Find the TextView in the list_item.xml layout with the ID default_text_view.
-        TextView f2016TextView = (TextView) listItemView.findViewById(R.id.f2016_text_view);
-        // Get the default translation from the currentPlace object and set this text on
-        // the default TextView.
-        f2016TextView.setText(String.valueOf(currentPlace.getFatalties2016()));
-
-        // Find the TextView in the list_item.xml layout with the ID default_text_view.
-        TextView causeTextView = (TextView) listItemView.findViewById(R.id.cause_text_view);
-        // Get the default translation from the currentPlace object and set this text on
-        // the default TextView.
-        causeTextView.setText(currentPlace.getCauseOfAccident());
-
         TextView gmapTextView = (TextView) listItemView.findViewById(R.id.gmap_text_view);
         // Get the default translation from the currentPlace object and set this text on
         // the default TextView.
-        causeTextView.setText(currentPlace.getLatitude()+ "/" + currentPlace.getLongitude());
+        gmapTextView.setText(currentPlace.getLatitude()+ "/" + currentPlace.getLongitude());
 
         // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
         // the ListView.
